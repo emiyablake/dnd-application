bbdf1ef3a58db59382e6ce1cfd1813325002caf9
emiyablake <maju.ads.02@gmail.com>
2024-04-10 16:13:59 -0300

    basic ui design

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World</h1>
       </header>
+
+      <body className="App-body">
+        <h3>What do you wanna know about Dungeons and Dragons?</h3>
+          <div className="App-search">
+            <label className="App-search-label">Search by name:</label>
+            <input className="App-search-input"></input>
+          </div>
+          <div className="App-btn-search">
+          <button className="btn-gameMech">Game Mechanics</button> 
+            <button className="btn-classes">Classes</button>
+            <button className="btn-races">Races</button>
+            <button className="btn-equip">Equipaments</button> 
+            <button className="btn-feats">Feats</button> 
+            <button className="btn-features">Features</button> 
+            <button className="btn-monsters">Monsters</button> 
+          </div>
+      </body>
     </div>
   );
 }
